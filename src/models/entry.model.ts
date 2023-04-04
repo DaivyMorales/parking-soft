7d3c37858311565a6b01b85c806fe07f229c104c
Daivy Morales <daivymorales7705@gmail.com>
2023-04-04 07:52:47 -0500

    Add db model entry

---
+++ src/models/entry.model.ts
@@ -0,0 +1,33 @@
+import { Schema, model, models } from "mongoose";
+
+const entrySchema = new Schema(
+  {
+    valero_num: {
+      type: String,
+      unique: true,
+      require: "The valero number is require",
+    },
+    plate: {
+      type: String,
+      unique: true,
+      require: "The valero number is require",
+    },
+    automobile_type: {
+      type: String,
+      require: "The automobile type is required",
+      trim: true,
+    },
+    amount: {
+      type: Number,
+      trim: true,
+    },
+  },
+  {
+    timestamps: {
+      currentTime: () => new Date().getTime() - 5 * 60 * 60 * 1000,
+    },
+    versionKey: false,
+  }
+);
+
+export default models.Entry || model("Entry", entrySchema);
